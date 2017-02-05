6adeb54ea9ea3b7f522306c3de9dc89eab94e66b
Peter Rose <peter@cookiedragongames.com>
2017-02-05 18:29:08 -0500

    All I wanted was a fucking triangle.

--- Core/Source/Main.cpp
+++ Core/Source/Main.cpp
@@ -15,6 +15,62 @@ void drawGraphics(GLFWwindow* window)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	//Testing Stuff
+	float points[] = {
+		0.0f,  0.5f,  0.0f,
+		0.5f, -0.5f,  0.0f,
+		-0.5f, -0.5f,  0.0f
+	};
+
+	//Magic OpenGL Nonsense I don't remotely understand. 
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+	//Shit I understand even less
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	//Magical Shader Bullshit
+	const char* vertex_shader =
+		"#version 400\n"
+		"in vec3 vp;"
+		"void main() {"
+		"  gl_Position = vec4(vp, 1.0);"
+		"}";
+
+	const char* fragment_shader =
+		"#version 400\n"
+		"out vec4 frag_colour;"
+		"void main() {"
+		"  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+		"}";
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	GLuint shader_programme = glCreateProgram();
+	glAttachShader(shader_programme, fs);
+	glAttachShader(shader_programme, vs);
+	glLinkProgram(shader_programme);
+
+	//The Magic
+	glUseProgram(shader_programme);
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	//End Testing
+
+
 	glfwSwapBuffers(window);
 }
 
@@ -39,7 +95,8 @@ GLFWwindow* setupGraphics()
 		return NULL;
 	}
 
-	//Make the window's context current 
+	//Make the window's context current
+	glewExperimental = GL_TRUE;
 	glfwMakeContextCurrent(window);
 
 	//Starting GLEW Extension Handler
@@ -93,7 +150,7 @@ int main()
 		drawGraphics(window);
 		if (processor.drawFlag)
 		{
-		//	drawGraphics(window);
+			//drawGraphics(window);
 		}
 
 		processor.setKeys();
@@ -103,6 +160,8 @@ int main()
 
 	}
 
+	//Close out everything and call it successful
+	glfwTerminate();
 	return EXIT_SUCCESS;
 }
 
