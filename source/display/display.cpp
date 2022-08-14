21f2e832e296cf4adc12b7128b54cf8b4d22f0f2
Peter Rose <peterrose1994@gmail.com>
2022-08-14 03:08:29 -0400

    Starting to work on QT Integration.

--- source/display/display.cpp
+++ source/display/display.cpp
@@ -65,14 +65,14 @@ bool display::initialize()
 
 	//SHADER SOURCE CODE
 	//------------------
-	char *vertexShaderSource = "#version 330 core\n"
+	const GLchar *vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
 		"void main()\n"
 		"{\n"
 		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 		"}\0";
 
-	char *fragmentShaderSource = "#version 330 core\n"
+    const GLchar *fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
 		"void main()\n"
 		"{\n"
