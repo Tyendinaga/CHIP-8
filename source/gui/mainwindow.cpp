a30a9ad0a5b2d2a64a4b1c9b0a8c47932f2e272b
Peter Rose <peterrose1994@gmail.com>
2022-08-20 01:19:58 -0400

    Showing QT popup on click.

--- source/gui/mainwindow.cpp
+++ source/gui/mainwindow.cpp
@@ -3,12 +3,12 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
+    // No Idea what this actually does.
     ui->setupUi(this);
 
 
-    //
+    // Trigger About QT Popup on Click
     connect(ui->actionAbout_Qt, &QAction::triggered, qApp, &QApplication::aboutQt);
-
 }
 
 MainWindow::~MainWindow() {
